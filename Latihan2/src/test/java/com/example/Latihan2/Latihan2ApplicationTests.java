91a2dd5c60d3107cecf885e87c799a6cca0b4a02
Feroli Bahru Al Rouf <62867862+ferolibahru@users.noreply.github.com>
2022-04-22 18:22:31 +0700

    Add files via upload

---
+++ Latihan2/src/test/java/com/example/Latihan2/Latihan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Latihan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
